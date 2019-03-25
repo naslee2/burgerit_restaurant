b657258c00da05e68594aa0bf1b7342feacaf89c
Nathan <naslee2@gmail.com>
2019-03-24 23:44:23 -0700

    adding code for animated scroll to new location

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 import classes from './index.css';
 
 $(document).ready(function() {
-console.log("index.js works!")
+console.log("index.js works!");
+    $( "a.scrollLink" ).click(function( event ) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
+    });
+
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top-59
+            }, 300);
+        }
+      });
+
 });
