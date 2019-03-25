49fb64645ed41d26f3d21843f5722dca57e6caa0
Nathan <naslee2@gmail.com>
2019-03-24 23:44:57 -0700

    adding code for animated scroll to new location

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ console.log("index.js works!");
         $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
     });
 
-    $('a[href^="#"]').on('click', function(event) {
-        var target = $(this.getAttribute('href'));
-        if( target.length ) {
-            event.preventDefault();
-            $('html, body').stop().animate({
-                scrollTop: target.offset().top-59
-            }, 300);
-        }
-      });
+    // $('a[href^="#"]').on('click', function(event) {
+    //     var target = $(this.getAttribute('href'));
+    //     if( target.length ) {
+    //         event.preventDefault();
+    //         $('html, body').stop().animate({
+    //             scrollTop: target.offset().top-59
+    //         }, 300);
+    //     }
+    //   });
 
 });
